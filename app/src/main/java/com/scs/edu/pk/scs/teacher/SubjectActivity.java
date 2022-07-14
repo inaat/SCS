f0225a52eead96b8d1ca333a29fa4455f9e8a66f
inaat <inayatullahkks@gmail.com>
2022-07-14 16:10:21 +0500

    SubjectActivity

---
+++ app/src/main/java/com/scs/edu/pk/scs/teacher/SubjectActivity.java
@@ -0,0 +1,15 @@
+package com.scs.edu.pk.scs.teacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.scs.edu.pk.scs.R;
+
+public class SubjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject);
+    }
+}
